98755e3501e58c9836b3d53e607f16c79d5307cd
arhanashik <ashik.pstu.cse@gmail.com>
2021-12-02 11:49:18 +0900

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Arhan Ashik on 2021/12/02.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
